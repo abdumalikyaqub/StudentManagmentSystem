14541fae1f990151e00328398a648dd28efed34c
Хидиров Абдумалик Абдумажидович <hidirovaa@kuzstu.ru>
2024-04-04 12:48:26 +0700

    last changes

--- StudentManagmentSystem/Models/Entities/Specialization.cs
+++ StudentManagmentSystem/Models/Entities/Specialization.cs
@@ -3,6 +3,8 @@
     public class Specialization
     {
         public int Id { get; set; }
+        public string? Code { get; set; }
         public string? Title { get; set; }
+        public string? Abbr { get; set; }
     }
 }
